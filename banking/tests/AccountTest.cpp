ae975296969438808a5afdffaa0e16997da2b87b
MORTI9 <159233913+MORTI9@users.noreply.github.com>
2025-05-21 20:34:03 +0300

    Update AccountTest.cpp

--- banking/tests/AccountTest.cpp
+++ banking/tests/AccountTest.cpp
@@ -2,14 +2,13 @@
 #include <gtest/gtest.h>
 
 using ::testing::Return;
+using ::testing::_;
 
 TEST(AccountTest, Initialization) {
     MockAccount account(1, 100);
     
-    EXPECT_CALL(account, id())
-        .WillOnce(Return(1));
-    EXPECT_CALL(account, GetBalance())
-        .WillOnce(Return(100));
+    EXPECT_CALL(account, id()).WillOnce(Return(1));
+    EXPECT_CALL(account, GetBalance()).WillOnce(Return(100));
     
     EXPECT_EQ(account.id(), 1);
     EXPECT_EQ(account.GetBalance(), 100);
@@ -18,13 +17,12 @@ TEST(AccountTest, Initialization) {
 TEST(AccountTest, ChangeBalance) {
     MockAccount account(1, 100);
     
-    EXPECT_CALL(account, id())
-        .WillOnce(Return(1));
-    EXPECT_CALL(account, Lock());
-    EXPECT_CALL(account, ChangeBalance(50));
-    EXPECT_CALL(account, GetBalance())
-        .WillOnce(Return(150));
-    EXPECT_CALL(account, Unlock());
+    // Устанавливаем ожидания в правильном порядке
+    EXPECT_CALL(account, id()).WillOnce(Return(1));
+    EXPECT_CALL(account, Lock()).Times(1);
+    EXPECT_CALL(account, ChangeBalance(50)).Times(1);
+    EXPECT_CALL(account, GetBalance()).WillOnce(Return(150));
+    EXPECT_CALL(account, Unlock()).Times(1);
     
     account.Lock();
     account.ChangeBalance(50);
