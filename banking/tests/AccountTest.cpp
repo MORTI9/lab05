420cbe9f45310af58ca2bd5b7b96f526f02b4ea4
MORTI9 <159233913+MORTI9@users.noreply.github.com>
2025-05-21 19:29:52 +0300

    Update AccountTest.cpp

--- banking/tests/AccountTest.cpp
+++ banking/tests/AccountTest.cpp
@@ -1,27 +1,15 @@
 #include "mocks/MockAccount.h"
 #include <gtest/gtest.h>
 
-TEST(AccountTest, Initialization) {
+TEST(AccountTest, InitialBalance) {
     MockAccount account(1, 100);
-    EXPECT_EQ(account.id(), 1);
     EXPECT_EQ(account.GetBalance(), 100);
-}
-
-TEST(AccountTest, LockUnlock) {
-    MockAccount account(1, 100);
-    
-    EXPECT_NO_THROW(account.Lock());
-    EXPECT_THROW(account.Lock(), std::runtime_error);
-    account.Unlock();
-    EXPECT_NO_THROW(account.Lock());
+    EXPECT_EQ(account.id(), 1);
 }
 
 TEST(AccountTest, ChangeBalance) {
     MockAccount account(1, 100);
-    
-    EXPECT_THROW(account.ChangeBalance(10), std::runtime_error);
     account.Lock();
     account.ChangeBalance(50);
     EXPECT_EQ(account.GetBalance(), 150);
-    account.Unlock();
 }
