99a77912abe1297c13369d241b4304827642d3c3
MORTI9 <159233913+MORTI9@users.noreply.github.com>
2025-05-21 18:32:25 +0300

    Update TransactionTest.cpp

--- banking/tests/TransactionTest.cpp
+++ banking/tests/TransactionTest.cpp
@@ -4,10 +4,8 @@
 
 using ::testing::_;
 using ::testing::Return;
-using ::testing::Throw;
 using ::testing::NiceMock;
 
-// Класс для тестирования protected методов
 class TestableTransaction : public Transaction {
 public:
     using Transaction::Credit;
@@ -53,13 +51,11 @@ TEST(TransactionTest, MakeSuccessScenario) {
     MockAccount to(2, 50);
     Transaction transaction;
 
-    // Ожидаем вызовы Lock/Unlock
     EXPECT_CALL(from, Lock()).Times(1);
     EXPECT_CALL(from, Unlock()).Times(1);
     EXPECT_CALL(to, Lock()).Times(1);
     EXPECT_CALL(to, Unlock()).Times(1);
 
-    // Настраиваем mock-методы
     EXPECT_CALL(from, GetBalance())
         .WillOnce(Return(200))
         .WillOnce(Return(200));
@@ -68,14 +64,13 @@ TEST(TransactionTest, MakeSuccessScenario) {
         .WillOnce(Return(50))
         .WillOnce(Return(150));
 
-    // Ожидаем изменения баланса
     EXPECT_CALL(from, ChangeBalance(-101)).Times(1);
     EXPECT_CALL(to, ChangeBalance(100)).Times(1);
 
     EXPECT_TRUE(transaction.Make(from, to, 100));
 }
 
-TEST(TransactionTest, CreditAddsToBalance) {
+TEST(TransactionTest, CreditTest) {
     TestableTransaction transaction;
     MockAccount account(1, 100);
 
@@ -83,37 +78,9 @@ TEST(TransactionTest, CreditAddsToBalance) {
     transaction.Credit(account, 50);
 }
 
-TEST(TransactionTest, DebitSuccessWhenEnoughBalance) {
+TEST(TransactionTest, DebitTest) {
     TestableTransaction transaction;
     MockAccount account(1, 100);
 
     EXPECT_CALL(account, GetBalance()).WillOnce(Return(100));
-    EXPECT_CALL(account, ChangeBalance(-50)).Times(1);
-    EXPECT_TRUE(transaction.Debit(account, 50));
-}
-
-TEST(TransactionTest, DebitFailsWhenNotEnoughBalance) {
-    TestableTransaction transaction;
-    MockAccount account(1, 50);
-
-    EXPECT_CALL(account, GetBalance()).WillOnce(Return(50));
-    EXPECT_FALSE(transaction.Debit(account, 100));
-}
-
-TEST(TransactionTest, SaveToDatabaseOutputsCorrectInfo) {
-    TestableTransaction transaction;
-    MockAccount from(1, 200);
-    MockAccount to(2, 50);
-
-    EXPECT_CALL(from, GetBalance()).WillOnce(Return(100));
-    EXPECT_CALL(to, GetBalance()).WillOnce(Return(150));
-
-    testing::internal::CaptureStdout();
-    transaction.SaveToDataBase(from, to, 100);
-    std::string output = testing::internal::GetCapturedStdout();
-
-    EXPECT_TRUE(output.find("1 send to 2 $100") != std::string::npos);
-    EXPECT_TRUE(output.find("Balance 1 is 100") != std::string::npos);
-    EXPECT_TRUE(output.find("Balance 2 is 150") != std::string::npos);
-}
-}
+    EXPECT_CALL
