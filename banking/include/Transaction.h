19408e1f58b2ad4a02b8d8268d2699d3a762b5ce
MORTI9 <159233913+MORTI9@users.noreply.github.com>
2025-05-21 20:46:39 +0300

    Update Transaction.h

--- banking/include/Transaction.h
+++ banking/include/Transaction.h
@@ -3,19 +3,19 @@
 class Account;
 
 class Transaction {
- public:
-  Transaction();
-  virtual ~Transaction();
+public:
+    Transaction();
+    virtual ~Transaction();
 
-  bool Make(Account& from, Account& to, int sum);
-  int fee() const { return fee_; }
-  void set_fee(int fee) { fee_ = fee; }
+    bool Make(Account& from, Account& to, int sum);
+    int fee() const { return fee_; }
+    void set_fee(int fee) { fee_ = fee; }
 
- protected:  // Изменили с private на protected для тестирования
-  virtual void Credit(Account& account, int sum);
-  virtual bool Debit(Account& account, int sum);
-  virtual void SaveToDataBase(Account& from, Account& to, int sum);
+protected:
+    virtual void Credit(Account& account, int sum);
+    virtual bool Debit(Account& account, int sum);
+    virtual void SaveToDataBase(Account& from, Account& to, int sum);
 
- private:
-  int fee_;
+private:
+    int fee_;
 };
