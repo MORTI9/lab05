75c5f397bbed47f47bb4184bb5b7ea7b7648cbe4
MORTI9 <159233913+MORTI9@users.noreply.github.com>
2025-05-21 20:34:24 +0300

    Update TransactionTest.cpp

--- banking/tests/TransactionTest.cpp
+++ banking/tests/TransactionTest.cpp
@@ -1,32 +1,33 @@
 #include "mocks/MockAccount.h"
-#include "mocks/MockTransaction.h"
 #include <gtest/gtest.h>
 
 using ::testing::Return;
 using ::testing::_;
-using ::testing::NiceMock;
 
 TEST(TransactionTest, MakeSuccessScenario) {
-    NiceMock<MockAccount> from(1, 200);
-    NiceMock<MockAccount> to(2, 50);
+    MockAccount from(1, 200);
+    MockAccount to(2, 50);
     Transaction transaction;
     
-    // Точные ожидания вызовов
+    // Правильные ожидания для from
+    EXPECT_CALL(from, id()).WillRepeatedly(Return(1));
     EXPECT_CALL(from, Lock()).Times(1);
-    EXPECT_CALL(from, Unlock()).Times(1);
-    EXPECT_CALL(to, Lock()).Times(1);
-    EXPECT_CALL(to, Unlock()).Times(1);
-    
-    // Ожидаем ровно 2 вызова GetBalance для каждого аккаунта
     EXPECT_CALL(from, GetBalance())
         .Times(2)
-        .WillRepeatedly(Return(200));
+        .WillOnce(Return(200))
+        .WillOnce(Return(200));
+    EXPECT_CALL(from, ChangeBalance(-101)).Times(1);
+    EXPECT_CALL(from, Unlock()).Times(1);
+    
+    // Правильные ожидания для to
+    EXPECT_CALL(to, id()).WillRepeatedly(Return(2));
+    EXPECT_CALL(to, Lock()).Times(1);
     EXPECT_CALL(to, GetBalance())
         .Times(2)
-        .WillRepeatedly(Return(50));
-    
-    EXPECT_CALL(from, ChangeBalance(-101)).Times(1);
+        .WillOnce(Return(50))
+        .WillOnce(Return(150));
     EXPECT_CALL(to, ChangeBalance(100)).Times(1);
+    EXPECT_CALL(to, Unlock()).Times(1);
     
     EXPECT_TRUE(transaction.Make(from, to, 100));
 }
