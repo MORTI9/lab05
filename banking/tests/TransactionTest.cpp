32bb75733091086cd7864b5f27eb2d4174c3d0fc
MORTI9 <159233913+MORTI9@users.noreply.github.com>
2025-05-21 20:08:15 +0300

    Update TransactionTest.cpp

--- banking/tests/TransactionTest.cpp
+++ banking/tests/TransactionTest.cpp
@@ -2,59 +2,31 @@
 #include "mocks/MockTransaction.h"
 #include <gtest/gtest.h>
 
-using ::testing::_;
 using ::testing::Return;
+using ::testing::_;
 using ::testing::NiceMock;
 
-class TestableTransaction : public Transaction {
-public:
-    using Transaction::Credit;
-    using Transaction::Debit;
-    using Transaction::SaveToDataBase;
-};
-
-TEST(TransactionTest, MakeThrowsWhenAccountsSame) {
-    NiceMock<MockAccount> from(1, 100);
-    NiceMock<MockAccount> to(1, 50);
-    Transaction transaction;
-    EXPECT_THROW(transaction.Make(from, to, 100), std::logic_error);
-}
-
-TEST(TransactionTest, MakeThrowsWhenSumNegative) {
-    NiceMock<MockAccount> from(1, 100);
-    NiceMock<MockAccount> to(2, 50);
-    Transaction transaction;
-    EXPECT_THROW(transaction.Make(from, to, -100), std::invalid_argument);
-}
-
-TEST(TransactionTest, MakeReturnsFalseWhenFeeTooHigh) {
-    NiceMock<MockAccount> from(1, 100);
-    NiceMock<MockAccount> to(2, 50);
-    Transaction transaction;
-    transaction.set_fee(60);
-    EXPECT_FALSE(transaction.Make(from, to, 100));
-}
-
 TEST(TransactionTest, MakeSuccessScenario) {
-    MockAccount from(1, 200);
-    MockAccount to(2, 50);
+    NiceMock<MockAccount> from(1, 200);
+    NiceMock<MockAccount> to(2, 50);
     Transaction transaction;
-
+    
+    // Точные ожидания вызовов
     EXPECT_CALL(from, Lock()).Times(1);
     EXPECT_CALL(from, Unlock()).Times(1);
     EXPECT_CALL(to, Lock()).Times(1);
     EXPECT_CALL(to, Unlock()).Times(1);
-
-    EXPECT_CALL(from, GetBalance())
-        .WillOnce(Return(200))
-        .WillOnce(Return(200));
     
+    // Ожидаем ровно 2 вызова GetBalance для каждого аккаунта
+    EXPECT_CALL(from, GetBalance())
+        .Times(2)
+        .WillRepeatedly(Return(200));
     EXPECT_CALL(to, GetBalance())
-        .WillOnce(Return(50))
-        .WillOnce(Return(150));
-
+        .Times(2)
+        .WillRepeatedly(Return(50));
+    
     EXPECT_CALL(from, ChangeBalance(-101)).Times(1);
     EXPECT_CALL(to, ChangeBalance(100)).Times(1);
-
+    
     EXPECT_TRUE(transaction.Make(from, to, 100));
 }
