b74caf09e44dddaa98a128c79f75964f137002a8
MORTI9 <159233913+MORTI9@users.noreply.github.com>
2025-05-21 20:07:53 +0300

    Update AccountTest.cpp

--- banking/tests/AccountTest.cpp
+++ banking/tests/AccountTest.cpp
@@ -1,15 +1,33 @@
 #include "mocks/MockAccount.h"
 #include <gtest/gtest.h>
 
+using ::testing::Return;
+using ::testing::_;
+
 TEST(AccountTest, Initialization) {
     MockAccount account(1, 100);
+    
+    // Устанавливаем ожидания для mock-методов
+    EXPECT_CALL(account, GetBalance())
+        .WillOnce(Return(100));
+    
     EXPECT_EQ(account.id(), 1);
     EXPECT_EQ(account.GetBalance(), 100);
 }
 
 TEST(AccountTest, ChangeBalance) {
     MockAccount account(1, 100);
+    
+    // Последовательность ожидаемых вызовов
+    ::testing::InSequence seq;
+    EXPECT_CALL(account, Lock());
+    EXPECT_CALL(account, ChangeBalance(50));
+    EXPECT_CALL(account, GetBalance())
+        .WillOnce(Return(150));
+    EXPECT_CALL(account, Unlock());
+    
     account.Lock();
     account.ChangeBalance(50);
     EXPECT_EQ(account.GetBalance(), 150);
+    account.Unlock();
 }
