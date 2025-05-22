1f0266077774fe51c106b7b89322a638952d063c
MORTI9 <159233913+MORTI9@users.noreply.github.com>
2025-05-22 12:49:48 +0300

    Update TransactionTest.cpp

--- banking/tests/TransactionTest.cpp
+++ banking/tests/TransactionTest.cpp
@@ -10,21 +10,18 @@ TEST(TransactionTest, MakeSuccessScenario) {
     MockAccount to(2, 50);
     Transaction transaction;
     
-    // Ожидания для from
-    EXPECT_CALL(from, id()).WillRepeatedly(Return(1));
+    // Для from
     EXPECT_CALL(from, Lock()).Times(1);
     EXPECT_CALL(from, GetBalance())
         .WillOnce(Return(200))
         .WillOnce(Return(200));
-    EXPECT_CALL(from, ChangeBalance(_)).Times(1);  // Исправленный вызов
+    EXPECT_CALL(from, ChangeBalance(_)).Times(1);
     EXPECT_CALL(from, Unlock()).Times(1);
     
-    // Ожидания для to
-    EXPECT_CALL(to, id()).WillRepeatedly(Return(2));
+    // Для to
     EXPECT_CALL(to, Lock()).Times(1);
-    EXPECT_CALL(to, GetBalance())
-        .WillOnce(Return(50));
-    EXPECT_CALL(to, ChangeBalance(_)).Times(1);  // Исправленный вызов
+    EXPECT_CALL(to, GetBalance()).WillOnce(Return(50));
+    EXPECT_CALL(to, ChangeBalance(_)).Times(1);
     EXPECT_CALL(to, Unlock()).Times(1);
     
     EXPECT_TRUE(transaction.Make(from, to, 100));
